af30d88e943cf2a535172fc79d5f4666eb31da81
shaharya53 <aryaaryashah@gmail.com>
2025-03-18 12:07:09 +0000

    done

---
+++ 01_basics/datatype_summary.js
@@ -0,0 +1,28 @@
+//primitive
+
+//b7 type: string,Number,Boolearn,null,undedined,symbol,BigInt
+
+const score=100;
+const scorevalue=null
+
+let useremail;
+const id=Symbol("123")
+const anotherid=Symbol("123")
+
+console.log(id==anotherid);
+
+const heros=["caption","spiderman","hulk"];
+
+let myobj={
+    name:"Arya",
+    age:"19"
+}
+//Reference (Non primitive)
+
+//Array,objects,Function
+
+const myfun= function(){
+    console.log("Hello world");
+    
+}
+
