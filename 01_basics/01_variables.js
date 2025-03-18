88e7877e503f14245fd034a5f37bee25ebb90e7b
shaharya53 <aryaaryashah@gmail.com>
2025-03-18 11:53:45 +0000

    done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,8 @@
+const accountId=31
+let accountEmail="aryashah@gmail.com"
+var accountpass="12345"
+accountcity="Ahmedabad"
+
+//  1accountId=2
+
+console.log(accountId)
---
+++ 01_basics/03_convertion.js
@@ -0,0 +1,26 @@
+// let score=undefined
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueINumber=Number(score)
+
+// console.log(typeof valueINumber)
+// console.log(valueINumber)
+
+// let isLoggedin=1
+// let bool=Boolean(isLoggedin)
+
+// console.log(bool);
+
+// --operation
+let value=3
+let neg=-value
+
+let str1="hello"
+let str2="bye"
+let str3=str1+str2
+console.log(str3)
+
+console.log(+true);
+console.log(+"");
